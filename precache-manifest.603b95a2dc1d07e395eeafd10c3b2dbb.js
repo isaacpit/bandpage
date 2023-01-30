f2b90b30c525f54dd7cd7e4146c81096dad3007d
isaacpitblado <isaacpitblado@tamu.edu>
2023-01-29 19:37:36 -0600

    Updates

---
+++ precache-manifest.603b95a2dc1d07e395eeafd10c3b2dbb.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = [
+  {
+    "revision": "32a59429fbe03b39bd48",
+    "url": "/bandpage-mrkatband.com/static/css/main.bc2a5968.chunk.css"
+  },
+  {
+    "revision": "32a59429fbe03b39bd48",
+    "url": "/bandpage-mrkatband.com/static/js/main.2fc1fccc.chunk.js"
+  },
+  {
+    "revision": "5c79c5beb375bb115cc4",
+    "url": "/bandpage-mrkatband.com/static/js/runtime~main.f9858ac1.js"
+  },
+  {
+    "revision": "7dad1ba20965a55a1a07",
+    "url": "/bandpage-mrkatband.com/static/css/2.3c283201.chunk.css"
+  },
+  {
+    "revision": "7dad1ba20965a55a1a07",
+    "url": "/bandpage-mrkatband.com/static/js/2.9ce183a1.chunk.js"
+  },
+  {
+    "revision": "4737dda17c37cc2d4030c2553599c3e6",
+    "url": "/bandpage-mrkatband.com/static/media/i-1.4737dda1.JPG"
+  },
+  {
+    "revision": "cec4ccb30e41198c7b0a5e117c55eb2b",
+    "url": "/bandpage-mrkatband.com/static/media/img_avatar1.cec4ccb3.png"
+  },
+  {
+    "revision": "f2686b2afdd63e515502de1b879abe0c",
+    "url": "/bandpage-mrkatband.com/static/media/la.f2686b2a.jpg"
+  },
+  {
+    "revision": "21b413cfb5a51f8c643ae1f06c7eb7aa",
+    "url": "/bandpage-mrkatband.com/static/media/mr-kat-logo.21b413cf.jpg"
+  },
+  {
+    "revision": "c653e77291403ed7d25f1385705e3b4c",
+    "url": "/bandpage-mrkatband.com/static/media/full-band-mohawk.c653e772.jpeg"
+  },
+  {
+    "revision": "9515875fda36c1fcf4ef1ab0fb7d0f88",
+    "url": "/bandpage-mrkatband.com/static/media/me.9515875f.jpeg"
+  },
+  {
+    "revision": "e66d38d0e8d470d71f3422450b256ebc",
+    "url": "/bandpage-mrkatband.com/static/media/visalogo.e66d38d0.jpg"
+  },
+  {
+    "revision": "1b3b350df76a30bbdfcfeb4981b4247f",
+    "url": "/bandpage-mrkatband.com/static/media/amexlogo.1b3b350d.png"
+  },
+  {
+    "revision": "dc93262058d964bb032c47411a882cd6",
+    "url": "/bandpage-mrkatband.com/static/media/tamulogo.dc932620.png"
+  },
+  {
+    "revision": "e2f2c8e6c75d45f259985d5159f1b51d",
+    "url": "/bandpage-mrkatband.com/static/media/aud.e2f2c8e6.jpeg"
+  },
+  {
+    "revision": "de1a07d9adea4ac16ca269ab4d20bc08",
+    "url": "/bandpage-mrkatband.com/static/media/sandbox-love.de1a07d9.mp4"
+  },
+  {
+    "revision": "af53c6950dbc95c01f9acc8ddbafe25d",
+    "url": "/bandpage-mrkatband.com/static/media/hrsb.af53c695.mp4"
+  },
+  {
+    "revision": "c6e3d89dafcd548592ee91fb9fd6ba57",
+    "url": "/bandpage-mrkatband.com/static/media/bastard.c6e3d89d.mp4"
+  },
+  {
+    "revision": "37ac9eccd64a81703aabb262aa6b5c08",
+    "url": "/bandpage-mrkatband.com/static/media/ccr.37ac9ecc.mp4"
+  },
+  {
+    "revision": "8b6aaaa051eca574e4df2dfead2bc92e",
+    "url": "/bandpage-mrkatband.com/static/media/teeth.8b6aaaa0.mp4"
+  },
+  {
+    "revision": "6477eb5458d1bd9732a451aa65d76069",
+    "url": "/bandpage-mrkatband.com/static/media/i-3.6477eb54.JPG"
+  },
+  {
+    "revision": "12069ac6f865473d676140bcf9c8523c",
+    "url": "/bandpage-mrkatband.com/static/media/i-2.12069ac6.JPG"
+  },
+  {
+    "revision": "9e35eceb8821ee56c7b697a180454c61",
+    "url": "/bandpage-mrkatband.com/static/media/bastard-end.9e35eceb.mp4"
+  },
+  {
+    "revision": "f3a53693fdbaf052e8f883c8fa2efcd6",
+    "url": "/bandpage-mrkatband.com/static/media/blog-post.1.f3a53693.md"
+  },
+  {
+    "revision": "d7c2d3aaf07b923ca18c6a107985b778",
+    "url": "/bandpage-mrkatband.com/static/media/city-streets.d7c2d3aa.jpeg"
+  },
+  {
+    "revision": "3b0ea00d8caca79573789a877dcf9255",
+    "url": "/bandpage-mrkatband.com/static/media/chicago.3b0ea00d.jpg"
+  },
+  {
+    "revision": "0b75fe318b19b6058540002261a4687a",
+    "url": "/bandpage-mrkatband.com/static/media/ny.0b75fe31.jpg"
+  },
+  {
+    "revision": "53df1f66a863768c08333b48a892884f",
+    "url": "/bandpage-mrkatband.com/static/media/ias-logo.53df1f66.jpg"
+  },
+  {
+    "revision": "6f580cd448bc2bd8ac4d0aa888d56044",
+    "url": "/bandpage-mrkatband.com/static/media/crowd.6f580cd4.mov"
+  },
+  {
+    "revision": "a9bc98ab26ecb68358adec4d7b1fa83d",
+    "url": "/bandpage-mrkatband.com/static/media/cover.a9bc98ab.jpeg"
+  },
+  {
+    "revision": "0be8b7a8ee62f6060115b23fca81f1c3",
+    "url": "/bandpage-mrkatband.com/static/media/i-k-1.0be8b7a8.JPG"
+  },
+  {
+    "revision": "3fd2c234561da0b2d9d8ac9fd7ac41b4",
+    "url": "/bandpage-mrkatband.com/static/media/k-v-j-1.3fd2c234.JPG"
+  },
+  {
+    "revision": "6b55fe1c4d1610d270070bf16891be7f",
+    "url": "/bandpage-mrkatband.com/static/media/v-2.6b55fe1c.JPG"
+  },
+  {
+    "revision": "83a7d71c8add1b9e1dd77c4bb522304a",
+    "url": "/bandpage-mrkatband.com/static/media/j-1.83a7d71c.JPG"
+  },
+  {
+    "revision": "1266491129eb2886c5f595eb1c3d5d16",
+    "url": "/bandpage-mrkatband.com/static/media/k-1.12664911.JPG"
+  },
+  {
+    "revision": "ed26c448640c562ab17d82a20ac8698c",
+    "url": "/bandpage-mrkatband.com/static/media/i-k-v-1.ed26c448.JPG"
+  },
+  {
+    "revision": "ea2744e6f177d8abfb401d5f74575d34",
+    "url": "/bandpage-mrkatband.com/static/media/v-k-1.ea2744e6.JPG"
+  },
+  {
+    "revision": "625c484a2825e52e008ec3b09ee1bd05",
+    "url": "/bandpage-mrkatband.com/static/media/j-2.625c484a.JPG"
+  },
+  {
+    "revision": "2a69a2f53042cde0817bada172758590",
+    "url": "/bandpage-mrkatband.com/static/media/i-k-3.2a69a2f5.JPG"
+  },
+  {
+    "revision": "fb44dfde22babe9a25fa929ce03f7523",
+    "url": "/bandpage-mrkatband.com/static/media/j-3.fb44dfde.JPG"
+  },
+  {
+    "revision": "8fa254ef096d25308c5e48af0af8c548",
+    "url": "/bandpage-mrkatband.com/static/media/j-k-1.8fa254ef.JPG"
+  },
+  {
+    "revision": "f3b145222e4f7c5678270f06c6c6b4cc",
+    "url": "/bandpage-mrkatband.com/static/media/bastard-mosh.f3b14522.mp4"
+  },
+  {
+    "revision": "1ddeb0cbd81851c9a123b5a19a7a9c6a",
+    "url": "/bandpage-mrkatband.com/static/media/wcc.1ddeb0cb.mp4"
+  },
+  {
+    "revision": "de9d9756202ce0e5e393209cca2d6f4c",
+    "url": "/bandpage-mrkatband.com/static/media/redrum.de9d9756.mp4"
+  },
+  {
+    "revision": "8a45b82606bcf6aa2544b96815f52edd",
+    "url": "/bandpage-mrkatband.com/static/media/u-mean-it.8a45b826.mp4"
+  },
+  {
+    "revision": "e05ba892665a4d98fea0e5ba3c5cb9a7",
+    "url": "/bandpage-mrkatband.com/static/media/mr-kat-at-101.e05ba892.jpeg"
+  },
+  {
+    "revision": "4ad16dba5fbd22b53f8da2c313e034d2",
+    "url": "/bandpage-mrkatband.com/static/media/sydney-climbing.4ad16dba.jpeg"
+  },
+  {
+    "revision": "df62caebbeed9dbf1094fdf1a885e88f",
+    "url": "/bandpage-mrkatband.com/static/media/teeth-demo-short.df62caeb.mov"
+  },
+  {
+    "revision": "71f9c2f0675d5c05bb8ebafd3cf28177",
+    "url": "/bandpage-mrkatband.com/static/media/cream-cheese-rangoon-demo-short.71f9c2f0.mov"
+  },
+  {
+    "revision": "1cca5cbb18d84801e4fe97badaede203",
+    "url": "/bandpage-mrkatband.com/static/media/redruM.1cca5cbb.mov"
+  },
+  {
+    "revision": "98ead49667bd80dbd0ebda7c24babd30",
+    "url": "/bandpage-mrkatband.com/static/media/bastard.98ead496.mov"
+  },
+  {
+    "revision": "b9d7603bbd8c9b2181a31d6d36a5e52c",
+    "url": "/bandpage-mrkatband.com/static/media/u-mean-it.b9d7603b.mov"
+  },
+  {
+    "revision": "11f96e70c1890ea7ab248aefc393ec28",
+    "url": "/bandpage-mrkatband.com/static/media/i cant win - double.11f96e70.mp4"
+  },
+  {
+    "revision": "9907353b7a86ac860f4338975818bc49",
+    "url": "/bandpage-mrkatband.com/static/media/teeth-solo.9907353b.mov"
+  },
+  {
+    "revision": "9bee71d643ecfe893861bed871a40e65",
+    "url": "/bandpage-mrkatband.com/static/media/teeth.9bee71d6.mov"
+  },
+  {
+    "revision": "828407d0822e5df8ea7cb59bdc8f4fc8",
+    "url": "/bandpage-mrkatband.com/static/media/mr-kat-sticker.828407d0.jpg"
+  },
+  {
+    "revision": "7ed879f8af22f0c211ca07f6e9439048",
+    "url": "/bandpage-mrkatband.com/static/media/sticker-laptop.7ed879f8.jpg"
+  },
+  {
+    "revision": "d0fa179ad745772f84f566c1210e6d2a",
+    "url": "/bandpage-mrkatband.com/static/media/dunk1.d0fa179a.mov"
+  },
+  {
+    "revision": "8b83e83d7d9a891b5030a0d33e9404ff",
+    "url": "/bandpage-mrkatband.com/static/media/dunk2.8b83e83d.mov"
+  },
+  {
+    "revision": "cf5c9d62fc5196d429a6da80b26cb016",
+    "url": "/bandpage-mrkatband.com/static/media/dunk3.cf5c9d62.mov"
+  },
+  {
+    "revision": "4c82612fae3d3e4270e0d020d65601f6",
+    "url": "/bandpage-mrkatband.com/static/media/aud2.4c82612f.jpeg"
+  },
+  {
+    "revision": "10aeddf93f25e72b4e8d8460200fe09e",
+    "url": "/bandpage-mrkatband.com/static/media/band-pic.10aeddf9.JPG"
+  },
+  {
+    "revision": "d9cd3b0b5a80d7d778fc6ee179864c5a",
+    "url": "/bandpage-mrkatband.com/static/media/paris.d9cd3b0b.jpeg"
+  },
+  {
+    "revision": "439794659a07bfc4dffc935407c4ab66",
+    "url": "/bandpage-mrkatband.com/static/media/friends-eating.43979465.jpg"
+  },
+  {
+    "revision": "be92f09c02d8560370ee5123e555fcf3",
+    "url": "/bandpage-mrkatband.com/static/media/portrait.be92f09c.jpeg"
+  },
+  {
+    "revision": "df5227c6e8928aea8cc1fb79dbd95264",
+    "url": "/bandpage-mrkatband.com/static/media/extendo.df5227c6.jpeg"
+  },
+  {
+    "revision": "99cbba3ba72c097ec32daa6f5eed33ec",
+    "url": "/bandpage-mrkatband.com/static/media/blog-post.2.99cbba3b.md"
+  },
+  {
+    "revision": "8d6e276be069ee011ba792969f01d327",
+    "url": "/bandpage-mrkatband.com/static/media/blog-post.3.8d6e276b.md"
+  },
+  {
+    "revision": "98a7aa52f6d368634e560d8649695917",
+    "url": "/bandpage-mrkatband.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5e0290864d5ee2599ff5dc7bf6c32e2.js"
+  "/bandpage-mrkatband.com/precache-manifest.603b95a2dc1d07e395eeafd10c3b2dbb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bandpage-mrkatband.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
